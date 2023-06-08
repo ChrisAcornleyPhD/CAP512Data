9d95ee72dd97e6b06f43aa8a787294990b4fb3c8
Christopher Acornley <pe02ca@uhi.ac.uk>
2023-06-08 10:21:24 +0100

    Collation script for counting number of Group A, Group B and Lecture classes attended

---
+++ scripts/collation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# open Grade export for each year
+    # extract column names (exclude USERNAME, GRADE, SYMBOL, ATTENDANCE TOTAL, ATTENDANCE PERCENT)
+    # Extract attendance data for all columns containing keyword 'Practical - Group B'
+        # Create new column - student total for group B
+    # Extract attendance data for all columns containing keyword 'Practical - Group A'
+        # Create new column - student total for group A
+    # Extract attendance data for all columns containing keyword 'Lecture'
+        # Create new column - student total for Lecture
+    
+    # Save df into new output 
+        # CollatedGradeExport.csv
